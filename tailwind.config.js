c662d0faa50d854ac35706e9bb75da710e6a295f
*Gustavo <gustavobbacco@gmail.com>
2024-11-18 20:32:44 -0300

    chore: installing tailwind in  project

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",               // Arquivo HTML
+    "./src/**/*.{js,ts,jsx,tsx}"  // Arquivos JS/TS/JSX/TSX dentro da pasta src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
